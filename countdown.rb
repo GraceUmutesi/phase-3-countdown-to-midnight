afa662f32263c24a10eebc716fe91d0a1fa45bd3
GraceUmutesi <tesigrace98@gmail.com>
2022-05-16 09:58:08 +0200

    assignment completed

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown secs_till00
+    while secs_till00>=1
+        puts "#{secs_till00} SECOND(S)!"
+        secs_till00 -=1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(secs_till00)
+    while secs_till00 > 0
+      puts "#{secs_till00} SECOND(S)!"
+      sleep(1)
+      secs_till00 -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
